56f1d10bc8ac150da31ccd5ad3d554341cc95a86
xiaoxingchen <chen-xx@zju.edu.cn>
2019-06-16 16:14:25 +0800

    Modify: finish chap 1

--- chap_1_introduction/task_2_update_of_quaternion_and_lie_algebra/main.cpp
+++ chap_1_introduction/task_2_update_of_quaternion_and_lie_algebra/main.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include "sophus/so3.h"
 
 int main()
 {
     Eigen::Matrix3d orientation_in_mat(Eigen::Matrix3d::Identity());
     Eigen::Quaterniond orientation_in_quat(Eigen::Quaterniond::Identity());
 
-    Eigen::Vector3d update;
-    update << 0.01, 0.02, 0.03;
-    Eigen::Matrix3d update_in_mat = Eigen::AngleAxisd(update.norm(), update).toRotationMatrix();
-    Eigen::Quaterniond update_in_quat(Eigen::AngleAxisd(update.norm(), update));
+    Eigen::Vector3d update_in_so3(0.01, 0.02, 0.03);
+    Eigen::Quaterniond update_in_quat(1, 0.01/2, 0.02/2, 0.03/2);
+    update_in_quat.normalize();
 
     int it_num = 20;
     for(int i = 0; i < it_num; i++)
     {
-        orientation_in_mat = update_in_mat * orientation_in_mat;
+        orientation_in_mat = Sophus::SO3::exp(update_in_so3).matrix() * orientation_in_mat;
         orientation_in_quat = update_in_quat * orientation_in_quat;
         std::cout << "step" << i << ": ";
         std::cout << "quat: " << orientation_in_quat.coeffs().transpose() << "\t\t";
